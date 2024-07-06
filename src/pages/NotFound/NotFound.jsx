a34cc7529e4ae6b00a9ace3a2e90bbe32817ecc7
Maksimka66 <maxfese@gmail.com>
2024-07-06 04:18:16 +0300

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,12 @@
-const NotFound = () => {};
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div>
+      <b>Oops! No pages with such request!</b>
+      <Link to="/">Back to home page</Link>;
+    </div>
+  );
+};
 
 export default NotFound;
