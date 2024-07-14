adcd37a3288006dae00e9258f63bdda23242142b
Maksimka66 <maxfese@gmail.com>
2024-07-14 22:04:28 +0300

    work with features component

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -10,23 +10,24 @@ const Features = () => {
     useSelector(selectOneCamper);
 
   return (
-    <div>
-      <ul className={css.detailsCamperList}>
-        <li>
-          <svg className={css.usersIcon} width="20" height="20">
-            <use href="/src/images/sprite/sprite.svg#icon-Users"></use>
-          </svg>
-          <p className={css.usersText}>{adults} adults</p>
-        </li>
-        <li>
-          <svg className={css.transIcon} width="20" height="20">
-            <use href="/src/images/sprite/sprite.svg#icon-Container"></use>
-          </svg>
-          <p className={css.transText}>
-            {transmission[0].toUpperCase() + transmission.substring(1)}
-          </p>
-        </li>
-        {/* <li>
+    <div className={css.featuresContainer}>
+      <div className={css.listsContainer}>
+        <ul className={css.detailsCamperList}>
+          <li>
+            <svg className={css.usersIcon} width="20" height="20">
+              <use href="/src/images/sprite/sprite.svg#icon-Users"></use>
+            </svg>
+            <p className={css.usersText}>{adults} adults</p>
+          </li>
+          <li>
+            <svg className={css.transIcon} width="20" height="20">
+              <use href="/src/images/sprite/sprite.svg#icon-Container"></use>
+            </svg>
+            <p className={css.transText}>
+              {transmission[0].toUpperCase() + transmission.substring(1)}
+            </p>
+          </li>
+          {/* <li>
           {details.airConditioner && (
             <>
               <svg className={css.airConditionerIcon} width="20" height="20">
@@ -36,80 +37,151 @@ const Features = () => {
             </>
           )}
         </li> */}
-        <li>
-          <svg className={css.engineIcon} width="20" height="20">
-            <use href="/src/images/sprite/sprite.svg#icon-Vector-3"></use>
-          </svg>
-          <p className={css.engineText}>
-            {engine[0].toUpperCase() + engine.substring(1)}
-          </p>
-        </li>
-        {details.kitchen && (
           <li>
-            <svg className={css.kitchenIcon} width="20" height="20">
-              <use href="/src/images/sprite/sprite.svg#icon-Vector-5"></use>
+            <svg className={css.engineIcon} width="20" height="20">
+              <use href="/src/images/sprite/sprite.svg#icon-Vector-3"></use>
             </svg>
-            <p className={css.kitchenText}>kitchen</p>
+            <p className={css.engineText}>
+              {engine[0].toUpperCase() + engine.substring(1)}
+            </p>
           </li>
-        )}
-        <li>
-          {details.beds === 1 ? (
-            <>
-              <svg className={css.bedIcon} width="20" height="20">
-                <use href="/src/images/sprite/sprite.svg#icon-Container-1"></use>
+          {details.kitchen && (
+            <li>
+              <svg className={css.kitchenIcon} width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Vector-5"></use>
               </svg>
-              <p className={css.bedsText}>{`${details.beds} bed`}</p>
-            </>
-          ) : (
-            <>
-              <svg className={css.bedIcon} width="20" height="20">
-                <use href="/src/images/sprite/sprite.svg#icon-Container-1"></use>
-              </svg>
-              <p className={css.bedsText}>{`${details.beds} beds`}</p>
-            </>
+              <p className={css.kitchenText}>kitchen</p>
+            </li>
           )}
-        </li>
-        {details.airConditioner && (
           <li>
-            <svg width="20" height="20">
-              <use href="/src/images/sprite/sprite.svg#con-streamline_hotel-air-conditioner"></use>
-            </svg>
-            <p>{details.airConditioner} air conditioner</p>
+            {details.beds === 1 ? (
+              <>
+                <svg className={css.bedIcon} width="20" height="20">
+                  <use href="/src/images/sprite/sprite.svg#icon-Container-1"></use>
+                </svg>
+                <p className={css.bedsText}>{`${details.beds} bed`}</p>
+              </>
+            ) : (
+              <>
+                <svg className={css.bedIcon} width="20" height="20">
+                  <use href="/src/images/sprite/sprite.svg#icon-Container-1"></use>
+                </svg>
+                <p className={css.bedsText}>{`${details.beds} beds`}</p>
+              </>
+            )}
           </li>
-        )}
-        {details.CD && (
-          <li>
-            <svg width="20" height="20">
-              <use href="/src/images/sprite/sprite.svg#icon-Group"></use>
-            </svg>
-            <p>CD</p>
-          </li>
-        )}
-        {details.radio && (
-          <li>
-            <svg width="20" height="20">
-              <use href="/src/images/sprite/sprite.svg#icon-Group-1"></use>
-            </svg>
-            <p>Radio</p>
-          </li>
-        )}
-        {details.hob && (
-          <li>
-            <svg>
-              <use href="/src/images/sprite/sprite.svg#icon-Group-2"></use>
-            </svg>
-            <p>{details.hob} hob</p>
-          </li>
-        )}
-        {details.TV && (
-          <li>
-            <svg>
-              <use href="/src/images/sprite/sprite.svg#icon-Group-2"></use>
-            </svg>
-            <p>{details.hob} hob</p>
-          </li>
-        )}
-      </ul>
+          {details.airConditioner && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#con-streamline_hotel-air-conditioner"></use>
+              </svg>
+              <p>{details.airConditioner} air conditioner</p>
+            </li>
+          )}
+          {details.CD && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Group"></use>
+              </svg>
+              <p>CD</p>
+            </li>
+          )}
+          {details.radio && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Group-1"></use>
+              </svg>
+              <p>Radio</p>
+            </li>
+          )}
+          {details.hob && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Group-2"></use>
+              </svg>
+              <p>{details.hob} hob</p>
+            </li>
+          )}
+          {details.TV && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Vertical-container"></use>
+              </svg>
+              <p>TV</p>
+            </li>
+          )}
+          {details.microwave && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Group-3"></use>
+              </svg>
+              <p></p>
+            </li>
+          )}
+          {details.freezer && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-arcticons_freezer"></use>
+              </svg>
+            </li>
+          )}
+          {details.shower && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Vector-4"></use>
+              </svg>
+            </li>
+          )}
+          {details.toilet && (
+            <li>
+              <svg width="20" height="20">
+                <use href="/src/images/sprite/sprite.svg#icon-Vector-7"></use>
+              </svg>
+            </li>
+          )}
+        </ul>
+        <div className={css.anotherDetailsContainer}>
+          <h3 className={css.vehicleDetailsHeader}>Vehicle details</h3>
+          <ul className={css.anotherDetailsList}>
+            <li>
+              <div>
+                <p>Form</p>
+                <p></p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>Length</p>
+                <p></p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>Width</p>
+                <p></p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>Height</p>
+                <p></p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>Tank</p>
+                <p></p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <p>Consumption</p>
+                <p></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
       <FormToOrder />
     </div>
   );
