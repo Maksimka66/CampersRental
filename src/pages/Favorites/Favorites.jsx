d373c08c2b9eb690bb6451dc120a0bef540dca9b
Maksimka66 <maxfese@gmail.com>
2024-07-18 02:49:43 +0300

    work with favorites

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,9 +1,29 @@
 import { useSelector } from "react-redux";
 
 import { selectFavorites } from "../../redux/campers/selectors";
+import Camper from "../../components/Camper/Camper";
+
+import css from "./Favorites.module.css";
 
 const Favorites = () => {
   const favoriteCampers = useSelector(selectFavorites);
+
+  return favoriteCampers.length > 0 ? (
+    <>
+      <h2 className={css.favoritesHeader}>Your favorite campers:</h2>
+      <ul className={css.favoritesList}>
+        {favoriteCampers.map((camper, index) => (
+          <li className={css.favoriteItem} key={index}>
+            <Camper camper={camper} />
+          </li>
+        ))}
+      </ul>
+    </>
+  ) : (
+    <h3 className={css.favoritesHeader}>
+      No campers have been added to favorites
+    </h3>
+  );
 };
 
 export default Favorites;
