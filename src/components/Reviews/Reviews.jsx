096abc9580be1aaf15bf59e0dc618c1f8e5650e5
Maksimka66 <maxfese@gmail.com>
2024-07-18 02:11:24 +0300

    work with stars rating

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -8,6 +8,14 @@ import css from "./Reviews.module.css";
 const Reviews = () => {
   const { reviews } = useSelector(selectOneCamper);
 
+  const starsRatingMarkup = (rating) => {
+    const stars = [];
+    for (let i = 1; i <= rating; i++) {
+      stars.push(i);
+    }
+    return stars;
+  };
+
   return (
     <div className={css.reviewsContainer}>
       <ul className={css.reviewList}>
@@ -15,9 +23,21 @@ const Reviews = () => {
           reviews.map((review, index) => (
             <li className={css.reviewListItem} key={index}>
               <div className={css.nameAndRatingContainer}>
-                <b>{review.reviewer_name[0]}</b>
-                <h3 className={css.reviewerName}>{review.reviewer_name}</h3>
-                <div>{review.reviewer_rating}</div>
+                <span className={css.firstLetter}>
+                  {review.reviewer_name[0]}
+                </span>
+                <div className={css.littleContainer}>
+                  <h3 className={css.reviewerName}>{review.reviewer_name}</h3>
+                  <ul className={css.ratingStarsList}>
+                    {starsRatingMarkup(review.reviewer_rating).map((i) => (
+                      <li key={i}>
+                        <svg width="16" height="16">
+                          <use href="/src/images/sprite/sprite.svg#icon-Rating"></use>
+                        </svg>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
               <p className={css.reviewText}>{review.comment}</p>
             </li>
